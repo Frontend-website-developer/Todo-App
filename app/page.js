df453b92399f7b238bc84a937f8cb3d03a189281
Frontend-website-developer <ssajidali.dev@gmail.com>
2025-01-29 18:34:58 -0800

    layout update

--- app/page.js
+++ app/page.js
@@ -23,23 +23,26 @@ const page = () => {
   }
 
   const completedTask = (i) => {
-    setiscompleted(!iscompleted);
-    console.log(iscompleted);
-  }
+    let copytask = [...taskwrapper]; // Copy the array
+    copytask[i].iscompleted = !copytask[i].iscompleted; // Toggle the boolean value
+    settaskwrapper(copytask); // Update state
+    console.log(copytask[i]); // Log the updated task
+};
+
 
   let renderTask = "No Task Available"
 
   if(taskwrapper.length>0){
       renderTask = taskwrapper.map(function(t,i){
     return (
-    <li key={i} className='mb-5'>
-    <div className='flex justify-between items-center'>
-      <h5>{t.title}</h5>
-      <h5>{t.description}</h5>
-      <button className='{iscompleted ? bg-green-900 : bg-red-900} text-white py-2 px-5 rounded' onClick={() => {
+    <li key={i} className='mb-5 p-5 border border-stone-800  mx-10 sm:mx-0'>
+    <div className='sm:flex justify-between items-center'>
+      <h5 className='font-semibold w-1/3 break-words mb-5 sm:mb-0'>{t.title}</h5>
+      <h5 className='font-medium w-1/3 break-words mb-5 sm:mb-0'>{t.description}</h5>
+      <button className={`${t.iscompleted ? "bg-green-900" : "bg-red-700"} text-white py-2 px-5 rounded hover:bg-red-600 mb-5 sm:mb-0`} onClick={() => {
         completedTask(i)
-      }}>{iscompleted ? "Completed" : "notcompleted"}</button>
-      <button className='bg-red-800 py-2 px-5 text-white rounded' onClick={ () => {
+      }}>{t.iscompleted ? "Completed" : "Not Completed"}</button>
+      <button className='bg-red-700 py-2 px-5 text-white rounded hover:bg-red-600' onClick={ () => {
         deleteTask(i)
         }}>Delete</button>
     </div>
@@ -53,20 +56,20 @@ const page = () => {
 
   return (
     <>
-      <h1 className='bg-black text-white text-center font-bold p-5 text-4xl'>My Todo List</h1>
-      <div className='container mx-auto'>
-        <form onSubmit={handleSubmit}>
-            <input type='text' className='text-xl border-zinc-800 border rounded-sm py-2 px-5 my-5 mr-5' placeholder='please enter task' value={title} onChange={(e) => {
+      <h1 className='bg-black text-white text-center font-bold py-10 px-5 text-4xl'>My Todo List</h1>
+      <div className='container mx-auto my-20'>
+        <form className='sm:flex justify-center items-center px-10 sm:px-0' onSubmit={handleSubmit}>
+            <input type='text' className='text-xl border-zinc-800 border rounded-sm py-3 px-5 my-5 mr-5 shadow shadow-zinc-600 drop-shadow' placeholder='please enter task' value={title} onChange={(e) => {
               settitle(e.target.value)
             }}></input>
-            <input type='text' className='text-xl border-zinc-800 border rounded-sm py-2 px-5 my-5 mr-5' placeholder='please description here' value={description} onChange={(e) => {
+            <input type='text' className='text-xl border-zinc-800 border rounded-sm py-3 px-5 my-5 mr-5 shadow shadow-zinc-600 drop-shadow' placeholder='please description here' value={description} onChange={(e) => {
               setdescription(e.target.value)
             }}></input>
-            <button className='bg-black text-white px-4 py-3 rounded text-xl'>Add Task</button>
+            <button className='bg-black text-white px-4 py-3 rounded text-xl hover:bg-slate-900'>Add Task</button>
 
         </form>
       </div>        
-        <div className='p-10 bg-gray-400'>
+        <div className='py-20 bg-gray-400'>
           <div className='container mx-auto'>
               <ul>
                 {renderTask}
